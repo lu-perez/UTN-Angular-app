d73f9e06fcf5f29a79c65b8248ba65e09889bd69
lu-perez <lu-perez@teco.com.ar>
2023-12-02 20:03:48 -0300

    toolbar role routes

--- src/app/core/modules/dashboard/components/nav/toolbar/toolbar.component.ts
+++ src/app/core/modules/dashboard/components/nav/toolbar/toolbar.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/modules/auth/services/auth.service';
+import { Role, SafeUser } from 'src/app/shared/types/types';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
+  currentUser!: SafeUser | null;
+  Role = Role;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser();
+  }
+
+  logOut(): void {
+    this.authService.logOut();
+    this.router.navigate(['/auth']);
+  }
 
 }
