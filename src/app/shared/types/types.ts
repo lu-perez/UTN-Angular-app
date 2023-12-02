c9bf21928c77ed20a13e43a46a0d6f5d395c4ced
lu-perez <lu-perez@teco.com.ar>
2023-12-02 12:19:07 -0300

    types

--- src/app/shared/types/types.ts
+++ src/app/shared/types/types.ts
@@ -1,8 +1,50 @@
 export type Game = {
+  id: number;
   name: string;
   cpuRequirements: string;
   memoryRequirements: string;
   storageRequirements: string;
   genre: string;
   price: string;
+}
+
+export type DLC = Omit<
+  Game,
+  'cpuRequirements' | 'memoryRequirements' | 'storageRequirements' | 'genre'
+> & {
+  relatedGameId: number;
+};
+
+export enum Role {
+  User = 'User',
+  Admin = 'Admin',
+  Agent = 'Agent',
+}
+
+export type User = {
+  id: number;
+  email: string;
+  password: string;
+  role: Role;
+}
+
+export enum PaymentMethod {
+  Cash = 'Cash',
+  CreditCard = 'Credit Card',
+}
+
+export type Purchase = {
+  id: number;
+  gameId: number;
+  userId: number;
+  paymentMethod: PaymentMethod;
+  pricePaid: string;
+}
+
+export type Lend = {
+  id: number;
+  gameId: number;
+  lenderUserId: number;
+  borrowerUserId: number;
+  expirationDate: Date;
 }
