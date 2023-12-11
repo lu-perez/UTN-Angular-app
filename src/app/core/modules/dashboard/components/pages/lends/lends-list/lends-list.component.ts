b914d1582abb2d2b816f52c5ee77f8ff45474c41
lu-perez <lu-perez@teco.com.ar>
2023-12-11 18:21:50 -0300

    lends list table, delete lend

--- src/app/core/modules/dashboard/components/pages/lends/lends-list/lends-list.component.ts
+++ src/app/core/modules/dashboard/components/pages/lends/lends-list/lends-list.component.ts
@@ -7,8 +7,12 @@ import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { DatePipe } from '@angular/common';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { ConfirmModalComponent } from 'src/app/shared/components/modal/confirm-modal/confirm-modal.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 interface LendsTable {
+  id: number;
   gameName: string;
   borrowerUserEmail: string;
   expirationDate: string | null;
@@ -28,7 +32,7 @@ export class LendsListComponent implements OnInit, AfterViewInit, OnDestroy {
   currentUser!: SafeUser | null;
   lends!: Lend[] | null;
   searchTerm!: string;
-  displayedColumns: string[] = ['gameName', 'borrowerUserEmail', 'expirationDate'];
+  displayedColumns: string[] = ['gameName', 'borrowerUserEmail', 'expirationDate', 'delete'];
   dataSource = new MatTableDataSource<LendsTable>(ELEMENT_DATA);
 
   lends$!: Observable<Lend[] | null>;
@@ -38,6 +42,8 @@ export class LendsListComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor(
     private lendsService: LendsService,
     private authService: AuthService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar,
   ) {
     this.currentUser = this.authService.getCurrentUser();
   }
@@ -51,6 +57,7 @@ export class LendsListComponent implements OnInit, AfterViewInit, OnDestroy {
       if (lends) {
         this.lends = lends;
         const data = lends.map((lend) => ({
+          id: lend.id,
           gameName: lend.game?.name || '',
           borrowerUserEmail: lend.user.email,
           expirationDate: new DatePipe('en-US').transform(lend.expirationDate, 'dd/MM/yyyy'),
@@ -77,6 +84,30 @@ export class LendsListComponent implements OnInit, AfterViewInit, OnDestroy {
 
   lendDetails(lend: Lend): void {
     console.log(lend);
+  }
+
+  deleteLend(lend: Lend) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      title: 'Confirm Delete Lend?',
+      message: 'Are you sure you want to delete this lend?',
+    };
+
+    const dialogRef = this.dialog.open(ConfirmModalComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.lendsService.deleteLend(lend.id).subscribe({
+          complete: () => {
+            this.dataSource.data = this.dataSource.data.filter(item => item.id !== lend.id);
+            this.snackBar.open('Lend deleted', '', { duration: 4000, panelClass: ['success-snackbar'] });
+          },
+          error: (err) => {
+            console.error(err);
+          }
+        });
+      }
+    });
   }
 
 }
